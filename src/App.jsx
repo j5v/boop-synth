1aeaeb33735370e482765086d624faa4a564345d
John Valentine <j5v@users.noreply.github.com>
2024-12-17 22:00:30 +0000

    components: header content footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Content from './components/Content.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <p className="read-the-docs">
-X
-      </p>
-    </>
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+import './Content.css'
+
+function Content() {
+
+  return (
+    <div className="Content">
+      Content
+    </div>
+  )
+}
+
+export default Content
