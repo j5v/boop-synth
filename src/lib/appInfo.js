d9a33c64a704cda6a1863dbd24fafa52e752b464
John Valentine <j5v@users.noreply.github.com>
2025-01-13 05:23:00 +0000

    inc versions

--- src/lib/appInfo.js
+++ src/lib/appInfo.js
@@ -1,8 +1,8 @@
 const appInfo = {
   appName: 'boop',
-  appVersion: '0.0.1.018',
-  appDate: '2025-01-09',
-  saveVersion: '0.0.1',
+  appVersion: '0.0.1.019',
+  appDate: '2025-01-13',
+  saveVersion: '0.0.2',
   specialVersionName: 'Early access',
   specialVersionAbout: 'Limited accessibility, no output parameter editing, some bugs known',
   authorName: 'j5v',
